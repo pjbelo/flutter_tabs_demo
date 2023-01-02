62457440c300c9778e6f37af56478c5293bb8412
Paulo Belo <pjbelo@gmail.com>
2023-01-02 16:57:35 +0000

    update step_3

--- lib/main.dart
+++ lib/main.dart
@@ -21,33 +21,59 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class TabsDemo extends StatelessWidget {
+class TabsDemo extends StatefulWidget {
   const TabsDemo({super.key});
+
+  @override
+  State<TabsDemo> createState() => _TabsDemoState();
+}
+
+class _TabsDemoState extends State<TabsDemo> {
+  int _selectedTab = 1;
+  final String _title = "Selected Tab: ";
+  String _fullTitle = "";
+
+  @override
+  void initState() {
+    _fullTitle = _title + _selectedTab.toString();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 3,
-      initialIndex: 2, // remember the first is 0
-      child: Scaffold(
-        appBar: AppBar(
-          bottom: const TabBar(
-            tabs: [
-              Tab(text: "Tab 0"),
-              Tab(text: "Tab 1"),
-              Tab(text: "Tab 2"),
+      initialIndex: _selectedTab,
+      child: Builder(builder: (BuildContext context) {
+        final TabController controller = DefaultTabController.of(context);
+        controller.addListener(() {
+          if (!controller.indexIsChanging) {
+            _selectedTab = controller.index;
+            setState(() {
+              _fullTitle = _title + _selectedTab.toString();
+            });
+          }
+        });
+        return Scaffold(
+          appBar: AppBar(
+            bottom: const TabBar(
+              tabs: [
+                Tab(text: "Tab 0"),
+                Tab(text: "Tab 1"),
+                Tab(text: "Tab 2"),
+              ],
+            ),
+            title: Text(_fullTitle),
+          ),
+          body: const TabBarView(
+            children: [
+              Center(child: Text('View 0')),
+              Center(child: Text('View 1')),
+              Center(child: Text('View 2')),
             ],
           ),
-          title: const Text('Tabs Demo'),
-        ),
-        body: const TabBarView(
-          children: [
-            Center(child: Text('View 0')),
-            Center(child: Text('View 1')),
-            Center(child: Text('View 2')),
-          ],
-        ),
-      ),
+        );
+      }),
     );
   }
 }
